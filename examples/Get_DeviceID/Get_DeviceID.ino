3804d25a35d3709abd02b9f859cf87f846b7a5f0
Irfan Indra Kurniawan <50232836+Vean@users.noreply.github.com>
2026-02-08 00:04:14 +0700

    Update Get_DeviceID.ino
    Update Commentary

--- examples/Get_DeviceID/Get_DeviceID.ino
+++ examples/Get_DeviceID/Get_DeviceID.ino
@@ -40,9 +40,20 @@ void setup()
     Serial.println("MAC   : " + info.mac);
     Serial.println("FLASH SIZE  : " + info.flashSize);
 
-    // EN: Device ID
-    // ID: Device ID
-    String deviceID = license.generateDeviceID(PRODUCT_SECRET);
+    /*
+    EN: Device ID is formed by combination of:
+    - Chip Model (Automatically taken according to ESP32 / ESP8266 device)
+    - MAC Address (Automatically taken from WiFi MAC Address, unique for each device)
+    - Flash Size (Optional, can be ignored if not needed change true / false on second parameter)
+    - Product Secret (You must define it yourself, different for each of your products)
+    ID: Device ID dapat bentuk dengan gambungan:
+    - Chip Model (Otomatis diambil sesuai perangkat ESP32 / ESP8266)
+    - MAC Address (Diambil otomatis dari WiFi MAC Address, unik untuk setiap perangkat)
+    - Flash Size (Opsi, dapat diabaikan jika tidak diperlukan ganti true / false pada parameter kedua)
+    - Product Secret (Anda harus mendefinisikan sendiri, berbeda untuk setiap produk Anda)
+    */
+
+    String deviceID = license.generateDeviceID(PRODUCT_SECRET, true);
     Serial.println();
     Serial.println("====== THIS IS DEVICE ID ======");
     Serial.println(deviceID);
